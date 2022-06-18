0493c2b65abc7946af38a6446b2a4a1d42b41e60
tickooayush007 <ayush.tickoo292000@gmail.com>
2022-06-18 19:37:18 +0530

    Spark SQL: Data Formatting done

--- src/main/scala/com/reazon/sparksql/NOAAData.scala
+++ src/main/scala/com/reazon/sparksql/NOAAData.scala
@@ -1,11 +1,30 @@
 package com.reazon.sparksql
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object NOAAData {
-  val spark: SparkSession = SparkSession.builder().master("local[*]").appName("NOAA Data DATA YT MARK3").getOrCreate()
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("NOAA Data DATA YT MARK3").getOrCreate()
 
-//  importing the implicits for reference
-  import spark.implicits._
+    //  importing the implicits for reference
+    import spark.implicits._
 
+    //    creating schema for DataFrame for formatting the Data
+    val tschema = StructType(Array(
+      StructField("sid", StringType),
+      StructField("date", DateType),
+      StructField("mtype", StringType),
+      StructField("value", DoubleType)
+    ))
+
+    val data2017: DataFrame = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv("C:\\Users\\ayush\\hadoop-3.2.2\\bin\\2017.csv")
+
+    data2017.show()
+//    data2017.schema.printTreeString()
+
+
+    spark.stop()
+
+  }
 }
