61a02b6c8f8a851a7f903acf59c4a292ccc6a915
tickooayush007 <ayush.tickoo292000@gmail.com>
2022-06-18 19:49:06 +0530

    Spark SQL: Experimenting Data Joins

--- src/main/scala/com/reazon/sparksql/NOAAData.scala
+++ src/main/scala/com/reazon/sparksql/NOAAData.scala
@@ -7,6 +7,9 @@ object NOAAData {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkSession.builder().master("local[*]").appName("NOAA Data DATA YT MARK3").getOrCreate()
 
+    //    in order to evade unnecessary info logs
+    spark.sparkContext.setLogLevel("WARN")
+
     //  importing the implicits for reference
     import spark.implicits._
 
@@ -20,9 +23,21 @@ object NOAAData {
 
     val data2017: DataFrame = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv("C:\\Users\\ayush\\hadoop-3.2.2\\bin\\2017.csv")
 
-    data2017.show()
-//    data2017.schema.printTreeString()
+    //    data2017.show()
+    //    data2017.schema.printTreeString()
 
+    //    limited the data for getting output with minimal configuration
+    val tmax2017 = data2017.filter($"mtype" === "TMAX")
+      .limit(1000).drop("mytpe")
+      .withColumnRenamed("value", "tmax")
+
+    val tmin2017 = data2017.filter('mtype === "TMIN")
+      .limit(1000).drop("mytpe")
+      .withColumnRenamed("value", "tmin")
+
+    //    joining temps together
+    val combinedTemps2017 = tmax2017.join(tmin2017, Seq("sid", "date"))
+    combinedTemps2017.show()
 
     spark.stop()
 
