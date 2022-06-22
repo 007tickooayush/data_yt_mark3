dd3c06c3e6024c6c9a3e84272d9cff6953c9dd0b
tickooayush007 <ayush.tickoo292000@gmail.com>
2022-06-22 17:35:55 +0530

    Spark SQL: Added ghcnd stations data and joined the data

--- src/main/scala/com/reazon/sparksql/NOAAData.scala
+++ src/main/scala/com/reazon/sparksql/NOAAData.scala
@@ -1,17 +1,23 @@
 package com.reazon.sparksql
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkFiles
+import org.apache.spark.sql.functions.avg
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object NOAAData {
   def main(args: Array[String]): Unit = {
 
     val spark: SparkSession = SparkSession.builder().master("local[*]").appName("NOAA Data DATA YT MARK3").getOrCreate()
 
+    val sc = spark.sparkContext
+
     //    in order to evade unnecessary info logs
-    spark.sparkContext.setLogLevel("WARN")
-    //    Logger.getLogger("org").setLevel(Level.FATAL)
+    sc.setLogLevel("WARN")
+
+    //    to suppress BlockManager Exceptions while execution
+    Logger.getLogger("org").setLevel(Level.FATAL)
 
     //  importing the implicits for reference
     import spark.implicits._
@@ -24,35 +30,65 @@ object NOAAData {
       StructField("value", DoubleType)
     ))
 
-    spark.sparkContext.addFile("src\\data\\2017.csv")
+    //LINK:
+    // https://www.ncei.noaa.gov/pub/data/ghcn/daily/
+    sc.addFile("src\\data\\2017.csv")
     val data2017: DataFrame = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv(SparkFiles.get("2017.csv"))
     //      .csv("src\\data\\2017.csv")
 
     //    data2017.show()
     //    data2017.schema.printTreeString()
 
+    //    sschema for station data
+    val sschema = StructType(Array(
+      StructField("sid", StringType),
+      StructField("lat", DoubleType),
+      StructField("lon", DoubleType),
+      StructField("name", StringType)
+    ))
+
+    sc.addFile("src\\data\\ghcnd-stations.txt")
+    val stationRDD = sc.textFile(SparkFiles.get("ghcnd-stations.txt")).map { line =>
+      val id = line.substring(0, 11)
+      val lat = line.substring(12, 20).toDouble
+      val lon = line.substring(21, 30).toDouble
+      val name = line.substring(41, 50)
+      Row(id, lat, lon, name)
+    }
+
+    //    using the custom created schema convert the RDD[Row] to DataFrame
+    val stations = spark.createDataFrame(stationRDD, sschema).cache()
+
     //    limited the data for getting output with minimal configuration
     val tmax2017 = data2017.filter($"mtype" === "TMAX")
+      .select('sid, 'date, 'value)
       .limit(1000)
-      //      .select('sid, 'date, 'value)
       //      .drop('mytpe)
       .withColumnRenamed("value", "tmax")
 
 
     val tmin2017 = data2017.filter('mtype === "TMIN")
+      .select('sid, 'date, 'value)
       .limit(1000)
-      //      .select('sid, 'date, 'value)
       //      .drop('mytpe)
       .withColumnRenamed("value", "tmin")
 
     //    joining temps together
     val combinedTemps2017 = tmax2017.join(tmin2017, Seq("sid", "date"))
-      .select('sid, 'date, 'tmax, 'tmin)
+    //      .select('sid, 'date, 'tmax, 'tmin)
     //    combinedTemps2017.show()
 
-//    calculating average temperature for each area code
-    val averageTemps2017 = combinedTemps2017.select('sid, 'date, ('tmax + 'tmin) / 20*1.8+32).withColumnRenamed("('tmax + 'tmin) / 20*1.8+32", "tavg")
-    averageTemps2017.show(20)
+    //    calculating average temperature for each area code
+    val dailyTemps2017 = combinedTemps2017.select('sid, 'date, ('tmax + 'tmin) / 20 * 1.8 + 32).withColumnRenamed("((((tmax + tmin) / 20) * 1.8) + 32)", "tavg")
+    //    dailyTemps2017.show(20)
+
+    val stationTemps2017 = dailyTemps2017.groupBy('sid).agg(avg('tavg))
+    //    stationTemps2017.show()
+
+    //    stations.show()
+
+    val joinedData2017 = stationTemps2017.join(stations, "sid")
+    joinedData2017.show()
 
     spark.stop()
 
