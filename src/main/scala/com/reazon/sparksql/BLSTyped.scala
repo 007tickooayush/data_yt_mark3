a3d3b365edd4f5619b4dd741c8fc661eca0fd418
tickooayush007 <ayush.tickoo292000@gmail.com>
2022-06-24 20:18:11 +0530

    Spark Pure SQL: Joined Typed Datasets

--- src/main/scala/com/reazon/sparksql/BLSTyped.scala
+++ src/main/scala/com/reazon/sparksql/BLSTyped.scala
@@ -2,9 +2,10 @@ package com.reazon.sparksql
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkFiles
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Encoders, SparkSession}
 
-case class Series(id: String, are: String, measure: String, title: String)
+case class Series(sid: String, are: String, measure: String, title: String)
 
 case class LAData(id: String, year: Int, period: String, value: Double)
 
@@ -27,24 +28,31 @@ object BLSTyped {
 
     // LINK BLS Data:
     // https://download.bls.gov/pub/time.series/la/
-    sc.addFile("src\\data\\la.data.64.County.txt")
+    sc.addFile("src\\data\\la.data.64.County.csv")
 
     val countyData = spark.read
       .schema(Encoders.product[LAData].schema) // fetch the schema for acccurate mapping
       .option("header", value = true)
       .option("delimiter", "\t")
-      .csv(SparkFiles.get("la.data.64.County.txt"))
+      .csv(SparkFiles.get("la.data.64.County.csv"))
+      .select(trim('id) as "id", 'year, 'period, 'value)
       .as[LAData] // import the data as specified in Case Class
-
+      .sample(withReplacement = false, 0.1) // sampling data
+      .cache()
     //    countyData.show()
 
     sc.addFile("src\\data\\la.series.tsv")
     val series = spark.read.textFile(SparkFiles.get("la.series.tsv")).map { line =>
-      val p = line.split("\t")
+      val p = line.split("\t").map(_.trim)
       Series(p(0), p(2), p(3), p(6))
     }.cache()
 
-    series.show()
+    //    series.show()
+
+    //    renamed the id to sid in Series and id from LAData using joinWith returns DataSet
+    val join1 = countyData.joinWith(series, 'id === 'sid)
+    //    join1.show()
+    println(join1.first())
 
     spark.stop()
   }
