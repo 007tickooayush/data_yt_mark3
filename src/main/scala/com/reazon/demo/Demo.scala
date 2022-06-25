940faafcb197e39bd9b051941b04dd0d84d4188d
tickooayush007 <ayush.tickoo292000@gmail.com>
2022-06-25 10:45:16 +0530

    Experimenting with Typed Datasets

---
+++ src/main/scala/com/reazon/demo/Demo.scala
@@ -0,0 +1,65 @@
+package com.reazon.demo
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Encoders, SparkSession}
+import org.apache.spark.{SparkContext, SparkFiles}
+
+case class CountyWeight(county_fips: Integer, value: Double)
+
+case class ZipData(
+                    zip: String,
+                    lat: Double,
+                    lng: Double,
+                    city: String,
+                    state_id: String,
+                    state_name: String,
+                    zcta: Boolean,
+                    parent_zcta: String,
+                    population: Integer,
+                    density: Double,
+                    county_fips: Integer,
+                    county_name: String,
+//                    county_weights: CountyWeight, // need nested mapping for accuracy
+                    county_names_all: String,
+                    county_fips_all: String,
+                    imprecise: Boolean,
+                    military: Boolean,
+                    timezone: String
+                  )
+
+object Demo {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder().master("local[*]").appName("DEMO Data Testing DATA YT MARK5").getOrCreate()
+    val sc: SparkContext = spark.sparkContext
+
+    //    in order to evade unnecessary info logs
+    sc.setLogLevel("WARN")
+
+    //    to suppress BlockManager Exceptions while execution
+    Logger.getLogger("org").setLevel(Level.FATAL)
+
+    //  importing the implicits for reference
+    import spark.implicits._
+    //    val zipSchema = StructType(Array(
+    //      StructField("zipCode",StringType),
+    //      StructField("lat",DoubleType),
+    //      StructField("lng",DoubleType),
+    //      StructField("population",IntegerType),
+    //      StructField("density",DoubleType)
+    //    ))
+
+    sc.addFile("src\\data\\uszips_raw.csv")
+    val zipHeaders = spark.read
+      .option("header", value = true)
+      //      .option("delimiter", ",")
+      //      .schema(zipSchema)
+            .schema(Encoders.product[ZipData].schema)
+      .csv(SparkFiles.get("uszips_raw.csv"))
+
+    zipHeaders.show(10)
+
+
+    spark.stop()
+  }
+}
